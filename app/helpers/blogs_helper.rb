307dc7fbe21e8dacdfc86df46d0e3d6819c5ff1d
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-17 15:28:56 +0900

    refactor: (バグ残留中)html_safeを利用しないように変更

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 module BlogsHelper
-  def format_content(blog)
-    h(blog.content).gsub(/\n|\r|\r\n/, "<br>").html_safe # rubocop:disable Rails/OutputSafety
-  end
 end
